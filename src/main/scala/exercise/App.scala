e10342ce65e72bd4459250b40929565202c385b8
Graeme Lockley <graemel@no9.co.za>
2016-07-22 08:14:28 +0200

    Moved the captureEntry out into a common private method and simplified a little further.

--- src/main/scala/exercise/App.scala
+++ src/main/scala/exercise/App.scala
@@ -1,73 +1,45 @@
 package exercise
 
 object App {
+	val positionsList = List("1", "2", "3", "4", "5", "6", "7", "8", "9")
+
 	def main(args: Array[String]): Unit = {
-		val positionsList = List("1", "2", "3", "4", "5", "6", "7", "8", "9")
 		val ticTacToe = new TicTacToe
 		ticTacToe.printTic()
 		println("This is just a positive scenario")
 		for (validEntryCount <- 0 until 9) {
-			if (validEntryCount % 2 == 0) {
-				def captureXEntry(): String = {
-					println("This is X's turn. Please enter the position-digit")
-					var entry: String = scala.io.StdIn.readLine()
-					var rightEntry = true
-					while (rightEntry) {
-						if (positionsList.contains(entry)) {
-							rightEntry = false
-							while (ticTacToe.isPositionTaken(entry)) {
-								println("Please re-enter a digit.This is already present")
-								entry = scala.io.StdIn.readLine()
-								while (entry == null || entry.isEmpty) {
-									println("Please do not hit enter without pressing any key")
-									entry = scala.io.StdIn.readLine()
-								}
-							}
-							while (!positionsList.contains(entry)) {
-								println("Please re-enter a digit")
-								entry = scala.io.StdIn.readLine()
-							}
-						} else {
-							println("Please re-enter a digit")
-							entry = scala.io.StdIn.readLine()
-						}
-					}
-					entry
-				}
-				val entry = captureXEntry()
-				ticTacToe.markPosition(entry.toInt, "X")
-			} else {
-				def captureYEntry(): String = {
-					println("This is O's turn. Please enter the position")
-					var entry: String = scala.io.StdIn.readLine()
-					var rightEntry = true
-					while (rightEntry) {
-						if (positionsList.contains(entry)) {
-							rightEntry = false
-							while (ticTacToe.isPositionTaken(entry)) {
-								println("Please re-enter a digit.This is already present")
-								entry = scala.io.StdIn.readLine()
-								while (entry == null || entry.isEmpty) {
-									println("Please do not hit enter without pressing any key")
-									entry = scala.io.StdIn.readLine()
-								}
-							}
-							while (!positionsList.contains(entry)) {
-								println("Please re-enter a digit")
-								entry = scala.io.StdIn.readLine()
-							}
-						} else {
-							println("Please re-enter a digit")
-							entry = scala.io.StdIn.readLine()
-						}
-					}
-					entry
-				}
-				val entry = captureYEntry()
-				ticTacToe.markPosition(entry.toInt, "O")
-			}
+			val turn = List("X", "O")(validEntryCount % 2)
+			val entry = captureEntry(ticTacToe, turn)
+			ticTacToe.markPosition(entry.toInt, turn)
 			ticTacToe.printTic()
 		}
 		println("Match draw")
+	}
+
+	private def captureEntry(ticTacToe: TicTacToe, symbol: String): String = {
+		println(s"This is $symbol's turn. Please enter the position-digit")
+		var entry: String = scala.io.StdIn.readLine()
+		var rightEntry = true
+		while (rightEntry) {
+			if (positionsList.contains(entry)) {
+				rightEntry = false
+				while (ticTacToe.isPositionTaken(entry)) {
+					println("Please re-enter a digit.This is already present")
+					entry = scala.io.StdIn.readLine()
+					while (entry == null || entry.isEmpty) {
+						println("Please do not hit enter without pressing any key")
+						entry = scala.io.StdIn.readLine()
+					}
+				}
+				while (!positionsList.contains(entry)) {
+					println("Please re-enter a digit")
+					entry = scala.io.StdIn.readLine()
+				}
+			} else {
+				println("Please re-enter a digit")
+				entry = scala.io.StdIn.readLine()
+			}
+		}
+		entry
 	}
 }
