6210f93481dafa79f7a43678eab7ea1f376d34d7
Graeme Lockley <graemel@no9.co.za>
2016-07-25 06:48:53 +0200

    Simplifed the status method and made it more functional in nature - the function is now much more understandable.

--- src/main/scala/exercise/TicTacToe.scala
+++ src/main/scala/exercise/TicTacToe.scala
@@ -1,7 +1,6 @@
 package exercise
 
-import scala.Option.empty
-import scala.collection.mutable
+import exercise.Position._
 
 class TicTacToe {
 	private val board = new Board()
@@ -14,29 +13,16 @@ class TicTacToe {
 
 	def status(): TicTacToeStatus = {
 		val strikes = Array(
-			Array(1, 2, 3), Array(4, 5, 6), Array(7, 8, 9),
-			Array(1, 4, 7), Array(2, 5, 8), Array(3, 6, 9),
-			Array(1, 5, 9), Array(3, 5, 7))
+			Array(One, Two, Three), Array(Four, Five, Six), Array(Seven, Eight, Nine),
+			Array(One, Four, Seven), Array(Two, Five, Eight), Array(Three, Six, Nine),
+			Array(One, Five, Nine), Array(Three, Five, Seven))
 
-		var result: Option[TicTacToeStatus] = empty
+		def hasWon(boardValue: BoardValue.Value): Boolean =
+			strikes.exists(strike => strike.forall(cell => board.get(cell).contains(boardValue)))
 
-		for (c <- strikes.indices if result.isEmpty) {
-			val count = mutable.HashMap(BoardValue.X -> 0, BoardValue.O -> 0)
-			val eachStrike = strikes(c)
-			for (b <- eachStrike.indices if result.isEmpty) {
-				val strike = Position(eachStrike(b) - 1)
-
-				def cellMarked(cellValue: BoardValue.Value): Unit = {
-					count(cellValue) = count(cellValue) + 1
-					if (count(cellValue) == 3) {
-						result = Option(GameWon(cellValue))
-					}
-				}
-
-				board.get(strike).foreach(cellMarked(_))
-			}
-		}
-
-		result.getOrElse(if (board.isFull) GameTie() else InProgress())
+		if (board.isFull) GameTie()
+		else if (hasWon(BoardValue.X)) GameWon(BoardValue.X)
+		else if (hasWon(BoardValue.O)) GameWon(BoardValue.O)
+		else InProgress()
 	}
 }
